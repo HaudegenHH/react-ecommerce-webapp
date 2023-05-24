8bf7a10e844ec4fc138dd2a6c713a850c517019b
HaudegenHH <einsundeins79@gmx.de>
2023-05-25 01:18:07 +0200

    google / email & password sign in

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,17 +1,41 @@
-import { signInWithGooglePopup } from '../../utils/firebase/firebase.utils';
+// import { useEffect } from 'react';
+// import { getRedirectResult } from 'firebase/auth';
+
+import { 
+    auth,
+    signInWithGooglePopup,
+    createUserDocumentFromAuth,
+    //signInWithGoogleRedirect,
+} from '../../utils/firebase/firebase.utils';
+
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
 
 const SignIn = () => {
+
+    // useEffect(() => {
+    //     const getResponse = async () => {
+    //       const response = await getRedirectResult(auth);
+     
+    //       if (response) {
+    //         const userDocRef = await createUserDocumentFromAuth(response.user);
+    //       }
+    //     };
+     
+    //     getResponse().catch(console.error);
+    // }, []);
+
     const logGoogleUser = async () => {
-        const response = await signInWithGooglePopup();
-        console.log(response);
-    } 
+        // console.log(response.user);
+        const { user } = await signInWithGooglePopup();
+        const userDocRef = await createUserDocumentFromAuth(user)    
+    }
 
     return (
         <div>
             <h1>Sign-in Page</h1>
-            <button onClick={logGoogleUser}>
-              Sign in with Google
-            </button>
+            <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+            {/*<button onClick={signInWithGoogleRedirect}>Sign in with Google Redirect</button>*/}
+            <SignUpForm />
         </div>
     );
 }
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -3,8 +3,20 @@ import {
     getAuth,
     GoogleAuthProvider, 
     signInWithRedirect, 
-    signInWithPopup, 
+    signInWithPopup,
+    createUserWithEmailAndPassword,
 } from 'firebase/auth';
+
+// just like firebase-app or firebase-auth, firestore is a different service.
+// with doc you get the specific document instance but
+// if you want get data from that document you will need getDoc and setDoc for 
+// setting the data for that document
+import { 
+    getFirestore,
+    doc,
+    getDoc,
+    setDoc,
+ } from 'firebase/firestore';
 
 const firebaseConfig = {
     apiKey: "AIzaSyAQt972WvnLec6uXMKU7yuJickTxoi4Ihw",
@@ -17,13 +29,68 @@ const firebaseConfig = {
   
 const firebaseApp = initializeApp(firebaseConfig);
 
-// specific configuration that google demands
 const provider = new GoogleAuthProvider();
+
+// specific configuration that google demands
 // means: every time somebody interacts with the provider, you want to 
 // always force them to select an account 
 provider.setCustomParameters({
-prompt: "select_account"
+    prompt: "select_account"
 })
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGoogleRedirect = () => signInWithRedirect(auth, provider);
+
+// db is a singleton instance
+export const db = getFirestore();
+
+// this fn should receive an user authentication object
+// ..so whatever Firebase returns you want to (partially) store in Firestore
+export const createUserDocumentFromAuth = async (userAuth, additionalInformation) => {
+    if(!userAuth) return;
+
+    // first you need to check if there is an existing doc reference.
+    // "Reference" is a special type of object that firestore uses, when
+    // talking about an instance of a document model.
+    // doc() takes in: the db instance, the name of the collection, and a
+    // unique identifier for that document
+    // conviently you'd take the uid from the firebase-auth response as the 
+    // identifier of the document
+    const userDocRef = doc(db, 'users', userAuth.uid);
+
+    // console.log(userDocRef);
+
+    const userSnapshot = await getDoc(userDocRef);
+
+    // console.log(userSnapshot);
+
+    // check based on a given document if any data / user with that uid exists
+    //console.log(userSnapshot.exists());
+
+    // if not, create that document (the reference was created above!)
+    if (!userSnapshot.exists()) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt,
+                ...additionalInformation
+            });
+        } catch (error) {
+            console.log(`error creating the user: ${error.message}`)
+        }
+    }
+
+    // finally return the reference to the document
+    return userDocRef;
+}
+
+//  create user with email and password
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+    if (!email || !password) return;
+    return await createUserWithEmailAndPassword(auth, email, password);
+}
